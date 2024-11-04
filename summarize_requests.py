c53e45e37a9e00763a470bf5a4831623acf36a27
Kathayut Kannasoot <97604415+Jdomenusrex@users.noreply.github.com>
2024-11-05 01:09:54 +0700

    testing summarize request

---
+++ summarize_requests.py
@@ -0,0 +1,214 @@
+# pip install pandas selenium requests beautifulsoup4 lxml
+
+import pandas as pd
+from selenium import webdriver
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pickle
+import time
+
+# Set up Chrome options
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument("--headless=new")  # Uncomment if you want to run in headless mode
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
+
+def initialize_model(Username,Password) :
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get("https://wordcount.com/login")
+    
+    username = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
+    password = driver.find_element(By.CSS_SELECTOR, 'input[name="Password"]')
+
+    username.send_keys("JdaKung@gmail.com")
+    password.send_keys("3xp{Kz7r2(rK")
+
+    time.sleep(1)
+
+    login_button = driver.find_elements(By.CLASS_NAME, "css-1oynujq-button")
+    login_button[1].click()
+
+    time.sleep(1)
+    # Find the span element by its class name
+    driver.get("https://wordcount.com/th/text-summarizer")
+
+    input("Press Enter to close the browser...")
+
+def summarize(Username,Password,content) :
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+
+    driver.get("https://wordcount.com/login")
+    
+    username = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
+    password = driver.find_element(By.CSS_SELECTOR, 'input[name="Password"]')
+
+    username.send_keys("JdaKung@gmail.com")
+    password.send_keys("3xp{Kz7r2(rK")
+
+    time.sleep(1)
+
+    login_button = driver.find_elements(By.CLASS_NAME, "css-1oynujq-button")
+    login_button[1].click()
+
+    time.sleep(1)
+    # Find the span element by its class name
+    driver.get("https://wordcount.com/th/text-summarizer")
+
+    print("Summarize model ready!")
+    textarea = driver.find_element(By.CSS_SELECTOR, 'textarea[class="css-1stoj7k-textarea"]')
+
+    # Enter text into the textarea
+    textarea.send_keys("เอ็นโซ มาเรสกา กุนซือ “สิงห์สำอาง”เชลซี ชาวอิตาเลียน ชื่นชมวัฒนธรรมที่เป็นเอกลักษณ์ของฟุตบอลอังกฤษ ว่าเกมตื่นเต้นเร้าใจของพรีเมียร์ลีกนั้นไม่มีที่ไหนในยุโรปจะเทียบได้ หลังจากเห็นเกมเมื่อสุดสัปดาห์ที่ผ่านมา ที่มีสจุดเปลี่ยนในช่วงทดเวลาหลายคู่ ทั้งชัยชนะ 4-3 ของเบรนท์ฟอร์ดเหนืออิปสวิช และชัยชนะ 2-1 ของเวสต์แฮมเหนือแมนเชสเตอร์ยูไนเต็ด ซึ่งเป็นแมตช์สุดท้ายที่เอริก เทน ฮากคุมผีแดง มาเรสกากล่าวว่า แฟนบอลในอังกฤษอาจมองข้ามความพิเศษนี้ไป โดยบอกว่า “นี่เป็นเหตุผลที่ผู้คนรักพรีเมียร์ลีก ในอังกฤษ เกมไม่เคยจบลงเลย ในประเทศอื่น ๆ อย่างประเทศของผม ถ้าคุณนำ 2-0 ในช่วง 5 นาทีสุดท้าย คุณอาจเห็นผู้เล่นที่ชะลอเกม และมันยากมากที่จะกลับมาได้ แต่ที่นี่มันเป็นวัฒนธรรมที่สวยงามที่ไม่เคยหยุดนิ่ง” ขณะที่ชัยชนะของเชลซี 2-1 เหนือนิวคาสเซิลเมื่อวันอาทิตย์ช่วยให้ทีมค่อย ๆ ไต่ขึ้นมาอยู่ที่อันดับ 5 ในตารางคะแนน และห่างเพียงหนึ่งแต้มจากโควต้าลุยแชมเปียนส์ลีก แม้ว่าจะมีแรงผลักดันอยู่ แต่ มาเรสกา ย้ำว่าเป้าหมายหลักของสโมสรคือการเติบโตในระยะยาวมากกว่าการเล็งอันดับในตอนนี้ โดยบอกว่า  “สโมสรไม่เคยพูดถึงการจบท็อปโฟร์ เป้าหมายคือการสร้างบางสิ่งที่สำคัญในอีกสี่ถึงห้าปีข้างหน้า”  และบอกด้วยว่าเขาชอบเน้นไปที่เกมต่อเกมมากกว่าที่จะมองไกลไปถึงปลายฤดูกาล รูป AFP")
+
+    buttons = driver.find_elements(By.TAG_NAME, 'button')
+    
+    buttons[2].click()
+
+    time.sleep(20)
+
+    textarea = driver.find_element(By.CLASS_NAME, "css-1stoj7k-textarea")
+
+    summary = textarea.text
+
+    print(summary)
+
+    driver.get("https://wordcount.com/th/text-summarizer")
+
+    time.sleep(2)
+
+    delete = driver.find_element(By.CSS_SELECTOR, 'button[class="css-1oynujq-button css-7xt92p-buttonIcon css-1cvsyjy"]')
+    delete.click()
+
+    # while(True):
+    #     pass
+
+    # summarize = driver.find_elements(By.CLASS_NAME, "css-1oynujq-button")
+    # summarize[3].click()
+
+    # username.clear()
+    # password.clear()
+
+    # username.send_keys(Username)
+    # password.send_keys(Password)
+
+    # agreeterm = driver.find_element(By.ID,"checky")
+    # agreeterm.click()
+
+    # getin = driver.find_element(By.CSS_SELECTOR,'input[type = "submit"]')
+
+    # getin.click()
+
+    # cookies = driver.get_cookies()
+
+    # session = requests.Session()  # Create a requests session
+    # for cookie in cookies:
+    #     session.cookies.set(cookie['name'], cookie['value'])
+
+    # search = driver.find_element(By.CSS_SELECTOR, 'li[class = "c-header__item c-header__item--padding c-header__item--pipe"]')
+    # search.click()
+
+    # search_text = driver.find_element(By.ID, 'keywords')
+    # search_text.clear()
+    # search_text.send_keys('Deep Learning')
+
+    # search_text.send_keys(u'\ue007')
+
+    # # Year Range
+    # startingyear = 2024
+    # endingyear = 2024
+
+    # while(startingyear <= endingyear) :
+
+    #     dismiss_cookie = driver.find_elements(By.CSS_SELECTOR, 'button[class = "c-cookie-banner__dismiss"]')
+    #     if(len(dismiss_cookie) != 0) : 
+    #         dismiss_cookie[0].click()
+
+    #     date = driver.find_element(By.CSS_SELECTOR, 'button[data-track-action = "date filter"]')
+    #     date.click()
+
+    #     date_range = driver.find_element(By.CSS_SELECTOR, 'a[data-test = "advance-search-link-date"]')
+    #     date_range.click()
+
+    #     start_year = driver.find_element(By.CSS_SELECTOR, 'select[name = "start_year"]')
+    #     select = Select(start_year)
+    #     select.select_by_visible_text(str(startingyear))
+
+    #     end_year = driver.find_element(By.CSS_SELECTOR, 'select[name = "end_year"]')
+    #     select = Select(end_year)
+    #     select.select_by_visible_text(str(startingyear))
+
+    #     search_button = driver.find_element(By.CSS_SELECTOR, 'button[class = "c-search__button c-search__button--width-auto"]')
+    #     search_button.click()
+
+    #     article_type = driver.find_element(By.CSS_SELECTOR, 'button[data-track-action = "article type filter"]')
+    #     article_type.click()
+
+    #     research_type = driver.find_element(By.ID, 'article-type-research')
+    #     research_type.click()
+
+    #     apply_filter = driver.find_element(By.CSS_SELECTOR, 'button.c-facet__submit')
+    #     apply_filter.submit()
+
+    #     latest = driver.find_element(By.ID, 'sort-by-date_desc')
+    #     latest.click()
+
+    #     count = 1
+
+    #     while(True) :
+    #         all_link = driver.find_elements(By.CSS_SELECTOR, 'a[data-track-action = "view article"]')
+
+    #         if len(all_link) == 0 :
+    #             search_return = driver.find_element(By.CSS_SELECTOR, 'button[class = "c-search__button"]')
+    #             search_return.click()
+
+    #             break
+
+    #         for link in all_link :
+    #             URL = link.get_attribute('href')
+    #             soup = fetch_data(URL,cache_path,session)
+    #             title = soup.find('h1', class_='c-article-title')
+    #             subjects = [subject.get_text(strip=True) for subject in soup.find_all('li', class_='c-article-subject-list__subject')]
+    #             authors = [author.get_text(strip=True) for author in soup.find_all('a', attrs={'data-test': 'author-name'})]
+    #             publish_date = parse_time(soup.find('time'))
+                
+    #             if title is not None and len(subjects) != 0 and len(authors) != 0 :
+    #                 data = { 
+    #                     'title' : title.text.strip(), 
+    #                     'subjects' : subjects, 
+    #                     'authors' : authors, 
+    #                     'publish-date' : publish_date
+    #                     }
+    #                 yield data
+
+    #             count += 1
+            
+    #         try:
+    #             next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li[data-page="next"]')))
+    #             next_button.click()
+    #         except:
+    #             search_return = driver.find_element(By.CSS_SELECTOR, 'button[class = "c-search__button"]')
+    #             search_return.click()
+
+    #             break
+            
+    #     startingyear += 1
+
+if __name__ == "__main__" :
+    # initialize_model(1,1)
+    summarize(1,2,3)
